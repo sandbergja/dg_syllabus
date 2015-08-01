485e3a722353b54892b96de076c46e5dea8faf20
Jane Sandberg <sandbej@linnbenton.edu>
2015-08-01 13:20:31 -0700

    getting rid of navbars on HTML version

--- compile_everything.sh
+++ compile_everything.sh
@@ -3,5 +3,5 @@ pdflatex dg-syllabus.tex
 pdflatex dg-syllabus.tex
 bibtex dg-reading-list.bib
 pdflatex dg-syllabus.tex
-latex2html dg-syllabus.tex -split +0 -dir html -local_icons
+latex2html dg-syllabus.tex -split +0 -dir html -local_icons -no_navigation
 latex2rtf dg-syllabus.tex
--- html/labels.pl
+++ html/labels.pl
@@ -2,15 +2,15 @@
 # Associate labels original text with physical files.
 
 
+$key = q/cite_oer/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_finra/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_gen-ed/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_oer/;
+$key = q/cite_worksheets/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -22,11 +22,11 @@ $key = q/cite_guillory2008s/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_rosenberg/;
+$key = q/cite_gen-ed/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_worksheets/;
+$key = q/cite_rosenberg/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -40,14 +40,6 @@ $noresave{$key} = "$nosave";
 # LaTeX2HTML 2008 (1.71)
 # labels from external_latex_labels array.
 
-
-$key = q/_newlabelxx/;
-$external_latex_labels{$key} = q|\ifx|; 
-$noresave{$key} = "$nosave";
-
-$key = q//;
-$external_latex_labels{$key} = q|\fi|; 
-$noresave{$key} = "$nosave";
 
 1;
 
