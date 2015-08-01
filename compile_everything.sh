245db9c242af499f400c8249aba457df73d4c513
Jane Sandberg <sandbej@linnbenton.edu>
2015-08-01 14:44:44 -0700

    fixing incorrect compile order

--- compile_everything.sh
+++ compile_everything.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pdflatex dg-syllabus.tex
-pdflatex dg-syllabus.tex
 bibtex dg-reading-list.bib
+pdflatex dg-syllabus.tex
 pdflatex dg-syllabus.tex
 latex2html dg-syllabus.tex -split +0 -dir html -local_icons -no_navigation
 latex2rtf dg-syllabus.tex
