1f9c38f357db8e898813391c439c29019e8e4719
Jane Sandberg <sandbej@linnbenton.edu>
2016-01-31 09:38:55 -0800

    adding guest lecture

--- html/labels.pl
+++ html/labels.pl
@@ -2,63 +2,7 @@
 # Associate labels original text with physical files.
 
 
-$key = q/cite_oer-dh/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_wellsley/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_worksheets/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_smart/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_finra/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_decipher/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_everyday/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_ooh/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_evaluation/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_eval/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_crosswalk/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_religion/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_rosenberg/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_finn2003just/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_training/;
+$key = q/cite_doyle/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -66,23 +10,11 @@ $key = q/cite_incorporating/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_credits/;
+$key = q/cite_eryk/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_lasers/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_duckworth/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_oer/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_tannahill/;
+$key = q/cite_decipher/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -90,11 +22,75 @@ $key = q/cite_gen-ed/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_sultan/;
+$key = q/cite_finra/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_doyle/;
+$key = q/cite_finn2003just/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_oer/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_smart/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_credits/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_rosenberg/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lasers/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_everyday/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_evaluation/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wellsley/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_tannahill/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_duckworth/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_religion/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_eval/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ooh/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_training/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_worksheets/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sultan/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -102,7 +98,11 @@ $key = q/cite_guillory2008s/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_eryk/;
+$key = q/cite_crosswalk/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_oer-dh/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
