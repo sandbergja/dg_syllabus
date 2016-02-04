0337f0aefb44e2fec115d6ff85c9fd3b84ff841b
Jane S <jane@localhost.localdomain>
2016-02-03 17:31:02 -0800

    Changing \howpublished to \note in @article citation

--- html/labels.pl
+++ html/labels.pl
@@ -1,36 +1,8 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2012 (1.2)
 # Associate labels original text with physical files.
 
 
-$key = q/cite_doyle/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_incorporating/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_eryk/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_decipher/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_gen-ed/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_finra/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_finn2003just/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_oer/;
+$key = q/cite_oer-dh/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -38,7 +10,15 @@ $key = q/cite_smart/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_credits/;
+$key = q/cite_everyday/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_guillory2008s/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_doyle/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -46,11 +26,19 @@ $key = q/cite_rosenberg/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_lasers/;
+$key = q/cite_eval/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_everyday/;
+$key = q/cite_incorporating/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ooh/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_eryk/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -58,7 +46,39 @@ $key = q/cite_evaluation/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_credits/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_religion/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gen-ed/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lasers/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_worksheets/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_training/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_decipher/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_wellsley/;
+$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sultan/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -70,31 +90,11 @@ $key = q/cite_duckworth/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_religion/;
+$key = q/cite_finn2003just/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_eval/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_ooh/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_training/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_worksheets/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_sultan/;
-$external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_guillory2008s/;
+$key = q/cite_finra/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
@@ -102,16 +102,20 @@ $key = q/cite_crosswalk/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_oer-dh/;
+$key = q/cite_oer/;
 $external_labels{$key} = "$URL/" . q|dg-syllabus.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2012 (1.2)
 # labels from external_latex_labels array.
 
+
+$key = q/_oldnewlabel/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
 
 1;
 
